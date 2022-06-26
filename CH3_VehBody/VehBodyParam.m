221e82b042acb1445fd36fa1db1a5996892dc4a3
SelectedCoder <32355000+MATLABCoder@users.noreply.github.com>
2022-06-26 21:19:05 +0800

    Part3

---
+++ CH3_VehBody/VehBodyParam.m
@@ -0,0 +1,13 @@
+% VehBody
+VehMass = 1200;%kg
+VehWhlperAxle = 2;% 
+VehFrontDis = 1.4;% m
+VehRearDis = 1.6; % m
+VehCGhight = 0.5;
+% Drag
+VehFrontalA = 3;%m^2
+VehDragCoeff = 0.4;
+AirDensity = 1.18;% kg/m^3
+% Pitch Dynamic Off
+% Variable
+VehSpdIni = 0;%m/s
